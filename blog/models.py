a08acb1a34f8fcd1235dabebf5a2d5d279a78b6b
Martin Oshaughnessy <vsredshift@gmail.com>
2021-02-23 14:08:24 +0100

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	#content = models.TextField()   # simple editor
+	content = RichTextField(blank=True, null=True)
+	date_posted = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	#likes = models.IntegerField(default=0)
+    #dislikes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('post-detail', kwargs={'pk': self.pk})
+
+class BlogCount(models.Model):
+    #fields you need
+    blog_views = models.IntegerField(default=0)
