27484d4045aeede7cad5da763fde82106e3dc320
vsredshift <vsredshift@gmail.com>
2021-02-23 15:26:19 +0100

    update settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = ')8&0rk999wb10uv!c@66zw0!z3i78rbn(_$mzzh+fbw)1%l*lp'
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
